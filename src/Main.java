37322a05c5aa68b00782fc68d372f0cbcc2cf4a7
Sergey Medvedev <msergejev@mail.ru>
2023-11-05 15:27:40 +0300

    update dz

--- src/Main.java
+++ src/Main.java
@@ -9,21 +9,19 @@ public class Main {
         // сумма пополнения порог, и для этих двух разных
         // сценариев рассчитываете сумму бонуса и выводите
         // на экран.
-        int initial_account = 100; //начальный счёт
-        int deposit_amount = 1500; // сумма пополнения
+        int initialAccount = 800; //начальный счёт
+        int depositAmount = 100; // сумма пополнения
 
-        int bonus_rubles;
-        if (deposit_amount > 1000){
-            bonus_rubles = deposit_amount / 100;
+        int bonusRubles;
+        if (depositAmount > 1000) {
+            bonusRubles = depositAmount / 100;
+        } else {
+            bonusRubles = 0;
         }
-        else
-            bonus_rubles = 0;
 
+        int balance = initialAccount + depositAmount + bonusRubles;
 
-        int balance = initial_account + deposit_amount + bonus_rubles;
-
-        System.out.println("Ваш счёт пополнен на " + deposit_amount + " рублей " + " на балансе: " + balance +", зачислено бонусных рублей " + bonus_rubles);
-
+        System.out.println("Ваш счёт пополнен на " + depositAmount + " рублей " + " на балансе: " + balance + ", зачислено бонусных рублей " + bonus_rubles);
 
 
     }
